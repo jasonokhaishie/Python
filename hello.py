7e3c3133ee9b6ccedb362159083ebb03c07b02bb
Jason <jasonokhaishie0@gmail.com>
2025-09-09 16:14:02 +0100

    feat tung

--- hello.py
+++ hello.py
@@ -3,3 +3,11 @@ name="walle"
 age = 12
 print(name)
 print(age)
+print("Am i a boy True or False?")
+is_a_boy =True
+print(is_a_boy)
+print("Am i a girl True or False?")
+is_a_girl =False
+print(is_a_girl)
+city = "Aberdeen"
+print(city)
