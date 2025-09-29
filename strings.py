9cd85315fd7f4ddd0fc248eb30f3999a85d2136f
Jason <jasonokhaishie0@gmail.com>
2025-09-29 17:02:42 +0100

    feat last name first name

---
+++ strings.py
@@ -0,0 +1,9 @@
+first_name = input("Enter your first name:  ")
+last_name = input ("Enter your last name:  ")
+
+#string concatenation
+full_name = first_name + " " + last_name
+print(full_name)
+print(first_name)
+first_letter=first_name[0]
+print(first_letter)
