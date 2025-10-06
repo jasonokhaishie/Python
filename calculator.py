21cd34ef4604b24ab44e98aee642463818ce067e
Jason <jasonokhaishie0@gmail.com>
2025-10-06 16:38:56 +0100

    feat arithmic

---
+++ calculator.py
@@ -0,0 +1,12 @@
+Number1 = int(input('Enter the number'))
+Number2 = int(input('Enter the number'))
+sum = Number1+Number2
+difference = Number1-Number2
+Multiply = Number1*Number2
+divide = Number1/Number2
+subtraction = Number1-Number2
+print(Multiply)
+print(sum)
+print(difference)
+print(divide)
+print()
