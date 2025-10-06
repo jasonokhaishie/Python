f85927b0629d47061c311dd49baa04a9dfafad00
Jason <jasonokhaishie0@gmail.com>
2025-10-06 17:30:12 +0100

    feat spelling

---
+++ string.py
@@ -0,0 +1,15 @@
+word = "Python"
+first_charecter = word[0]
+second_charecter = word[1]
+third_charecter = word[2]
+forth_charecter = word[3]
+fifth_charecter = word[4]
+sixth_charecter = word[5]
+print(word)
+print(first_charecter)
+print(second_charecter)
+print(third_charecter)
+print(forth_charecter)
+print(fifth_charecter)
+print(sixth_charecter)
+print(word[1:])
