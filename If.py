2e9afb8f2cc8a05f438cd13928cef4f6965ba953
Jason <jasonokhaishie0@gmail.com>
2025-10-13 15:39:54 +0100

    feat else

---
+++ If.py
@@ -0,0 +1,9 @@
+age= int(input("how old are you? "))
+if age>=18:
+ print("You are allowed to vote")
+else:
+ print("You are not allowed to vote")
+else:
+print("You are almost allowed to vote")
+else:
+ print("You are  allowed to vote")
