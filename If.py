83ff2085bce318cea6691e7dedb92e550e71062b
Jason <jasonokhaishie0@gmail.com>
2025-10-14 17:08:51 +0100

    feat git

--- If.py
+++ If.py
@@ -1,9 +1,12 @@
 age= int(input("how old are you? "))
 if age>=18:
- print("You are allowed to vote")
+  print("You are allowed to vote")
+  print("yes")
 else:
- print("You are not allowed to vote")
-else:
-print("You are almost allowed to vote")
-else:
- print("You are  allowed to vote")
+  print("You are not allowed to vote")
+print("Hello " *100)
+for i in range(10, -1, -1):
+  if i == 0:
+    print("Happy New Year!  🥳")
+  else:
+    print(i)
